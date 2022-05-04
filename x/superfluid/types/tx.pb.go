394dd1762b685f31491a5100470890aed73dda1d
John Letey <john@kyve.network>
2022-05-04 14:28:48 +0100

    chore: rebuild chain

--- x/superfluid/types/tx.pb.go
+++ x/superfluid/types/tx.pb.go
@@ -178,6 +178,7 @@ const _ = grpc.SupportPackageIsVersion4
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
 type MsgClient interface {
+	// SuperfluidFund ...
 	SuperfluidFund(ctx context.Context, in *MsgSuperfluidFund, opts ...grpc.CallOption) (*MsgSuperfluidFundResponse, error)
 }
 
@@ -200,6 +201,7 @@ func (c *msgClient) SuperfluidFund(ctx context.Context, in *MsgSuperfluidFund, o
 
 // MsgServer is the server API for Msg service.
 type MsgServer interface {
+	// SuperfluidFund ...
 	SuperfluidFund(context.Context, *MsgSuperfluidFund) (*MsgSuperfluidFundResponse, error)
 }
 
