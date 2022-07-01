0a2e65d051cf47c20968d872cb026fb85b1fc0f3
ruslanglaznyov <ruslan.glaznyov@gmail.com>
2022-07-01 19:20:34 +0300

    feature: update tests to v0.6.0 chore: tests with sdk v2

--- test/src/helpers/utils.ts
+++ test/src/helpers/utils.ts
@@ -1,26 +1,26 @@
-import axios from "axios";
-import BigNumber from "bignumber.js";
 import { spawn, ChildProcessWithoutNullStreams } from "child_process";
-import { BASE_URL } from "./constants";
+import { NETWORK } from "./constants";
+import KyveSDK from "@kyve/sdk";
 
 export const sleep = (milliseconds: number) => {
   return new Promise((resolve) => setTimeout(resolve, milliseconds));
 };
-
+declare global {
+  var chain: ChildProcessWithoutNullStreams;
+}
 export const startChain = (): Promise<ChildProcessWithoutNullStreams> => {
   return new Promise<ChildProcessWithoutNullStreams>((resolve, reject) => {
     console.log("Starting chain on localhost ...");
     console.log("This may take up to a minute");
 
-    const chain = spawn("starport", ["chain", "serve", "--reset-once"]);
+    global.chain = spawn("ignite", ["chain", "serve", "--reset-once"]);
 
     chain.stdout.on("data", (data: Buffer) => {
-      if (data.toString().includes(`Blockchain API: ${BASE_URL}`)) {
-        console.log(`Chain started on ${BASE_URL}`);
-        setTimeout(() => resolve(chain), 5000);
+      if (data.toString().includes(`Token faucet`)) {
+        console.log(`Chain started`);
+        setTimeout(() => resolve(chain), 1000);
       }
     });
-
     chain.stderr.on("data", () => {
       reject();
     });
@@ -30,47 +30,18 @@ export const startChain = (): Promise<ChildProcessWithoutNullStreams> => {
     });
   });
 };
-
-export const getBalanceByAddress = async (
-  address: string
-): Promise<BigNumber> => {
-  const { data } = await axios.get(
-    `${BASE_URL}/cosmos/bank/v1beta1/balances/${address}/by_denom?denom=tkyve`
-  );
-  return new BigNumber(data.balance.amount);
+export const restartChain = () => {
+  return new Promise<ChildProcessWithoutNullStreams>((resolve) => {
+    global.chain.on("close", function test() {
+      setTimeout(async () => {
+        await startChain();
+        global.chain.removeAllListeners("close");
+        resolve(global.chain);
+      }, 1000);
+    });
+    global.chain.kill();
+  });
 };
 
-export const getPoolById = async (id: string | number): Promise<any> => {
-  const { data } = await axios.get(
-    `${BASE_URL}/kyve/registry/v1beta1/pool/${id}`
-  );
-  return data.pool;
-};
-
-export const getFundersList = async (id: string | number): Promise<any[]> => {
-  const { data } = await axios.get(
-    `${BASE_URL}/kyve/registry/v1beta1/funders_list/${id}`
-  );
-  return data.funders;
-};
-
-export const getStakersList = async (id: string | number): Promise<any[]> => {
-  const { data } = await axios.get(
-    `${BASE_URL}/kyve/registry/v1beta1/stakers_list/${id}`
-  );
-  return data.stakers;
-};
-
-export const getDelegationPoolData = async (
-  staker: string,
-  poolId: string | number
-): Promise<any> => {
-  try {
-    const { data } = await axios.get(
-      `${BASE_URL}/kyve/registry/v1beta1/delegators_by_pool_and_staker/${poolId}/${staker}`
-    );
-    return data.delegation_pool_data;
-  } catch (e) {
-    return undefined;
-  }
-};
+export const localSdk = new KyveSDK(NETWORK);
+export const lcdClient = localSdk.createLCDClient();
--- test/src/kyve.test.ts
+++ test/src/kyve.test.ts
@@ -1,32 +1,27 @@
-import { ChildProcessWithoutNullStreams } from "child_process";
 import { funding } from "./funding";
-import { sleep, startChain } from "./helpers/utils";
+import { startChain } from "./helpers/utils";
 import { staking } from "./staking";
+import { alice, bob, charlie } from "./helpers/accounts";
 import { delegation } from "./delegation";
-
 describe("chain", () => {
   // disable timeout
   jest.setTimeout(24 * 60 * 60 * 1000);
 
-  // define chain process
-  let chain: ChildProcessWithoutNullStreams;
-
   beforeAll(async () => {
-    // start local chain
-    chain = await startChain();
-
-    await sleep(5000);
+    await startChain();
+    await alice.init();
+    await bob.init();
+    await charlie.init();
   });
 
   // funding
   describe("Funding", funding);
   // staking
   describe("Staking", staking);
-  // delegation
+  // // delegation
   describe("Delegation", delegation);
-
   afterAll(() => {
     // stop local chain
-    chain.kill();
+    global.chain.kill();
   });
 });
