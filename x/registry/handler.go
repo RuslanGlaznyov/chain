d8f401d6c6df7e26c1240322582a3f3e470f1f50
mbreithecker <m@breithecker.de>
2022-07-08 17:40:07 +0200

    feat: implement CommissionChange queue + separate transaction

--- x/registry/handler.go
+++ x/registry/handler.go
@@ -56,6 +56,9 @@ func NewHandler(k keeper.Keeper) sdk.Handler {
 		case *types.MsgUpdateMetadata:
 			res, err := msgServer.UpdateMetadata(sdk.WrapSDKContext(ctx), msg)
 			return sdk.WrapServiceResult(ctx, res, err)
+		case *types.MsgUpdateCommission:
+			res, err := msgServer.UpdateCommission(sdk.WrapSDKContext(ctx), msg)
+			return sdk.WrapServiceResult(ctx, res, err)
 			// this line is used by starport scaffolding # 1
 		default:
 			errMsg := fmt.Sprintf("unrecognized %s message type: %T", types.ModuleName, msg)
