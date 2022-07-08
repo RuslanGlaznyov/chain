7b375872a43c3fcaf7e80177142c7fbb870b08dd
mbreithecker <m@breithecker.de>
2022-07-08 11:24:33 +0200

    feat: add staker migration

--- app/upgrades/v0.6.0/upgrades.go
+++ app/upgrades/v0.6.0/upgrades.go
@@ -2,10 +2,20 @@ package v0_6_0
 
 import (
 	registrykeeper "github.com/KYVENetwork/chain/x/registry/keeper"
+	"github.com/KYVENetwork/chain/x/registry/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
 )
+
+func migrateStakers(registryKeeper *registrykeeper.Keeper, ctx sdk.Context) {
+
+	for _, staker := range registryKeeper.GetAllStaker(ctx) {
+		staker.Status = types.STAKER_STATUS_ACTIVE
+		registryKeeper.GetStaker(ctx, staker.Account, staker.PoolId)
+	}
+
+}
 
 func CreateUpgradeHandler(
 	registryKeeper *registrykeeper.Keeper,
@@ -13,7 +23,9 @@ func CreateUpgradeHandler(
 	return func(ctx sdk.Context, plan upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
 
 		registryKeeper.UpgradeHelperV060MigrateSecondIndex(ctx)
-		// Return.
+
+		migrateStakers(registryKeeper, ctx)
+
 		return vm, nil
 	}
 }
